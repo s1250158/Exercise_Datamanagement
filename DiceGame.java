43b042417b11a19fee89ab0e2bc30a35300927f7
s1250158 <s1250158@u-aizu.ac.jp>
2019-07-10 22:49:45 +0900

    Completed

--- DiceGame.java
+++ DiceGame.java
@@ -1,5 +1,6 @@
 import java.util.Random;
 import java.io.*;
+import java.util.Scanner;
 
     class Dice{
 
@@ -43,12 +44,7 @@ class DiceGame{
             e.printStackTrace();
         }
 	
-        System.out.println("Hello, " + str + "!\n");
-
-
-
-
-	
+        System.out.println("Hello, " + str + "!\n");	
 	System.out.println("Rolling the dice...");
 
 	Dice d1=new Dice(1);
@@ -62,8 +58,22 @@ class DiceGame{
 	System.out.println("Total value: "+sum);
 
 	if(sum>7){
-	    System.out.println(str+" won!");}
-	else {System.out.println(str+"lost.");}
+	    System.out.println(str+" won!\n\n\n\n");}
+	else {System.out.println(str+"lost.\n\n\n\n");}
+
+	System.out.println("I will roll the dice until the total reaches 15\n");
+
+
+	int sum1=0;
+	int count=0;
+	while(sum1<15){
+	    count++;
+	    Dice d3=new Dice(3);
+	    sum1=sum1+d3.shake();
+	    System.out.println("Die["+count+"] : "+d3.spot);
+	    System.out.println("Sum is "+sum1);
+	}
+        System.out.println("\nI rolled the dice "+count+" times before exceeding 15.");
 	
     }
 }
